4b4f8335058959d6e7dbe27a6176ee1f962c46cb
Paul Tuck <paul.tuck@oxon.org>
2023-01-04 13:25:41 +0700

    "2023-01-04_13:25_UTC+07_Wed"

--- executors/temperature.py
+++ executors/temperature.py
@@ -1,12 +1,11 @@
+from decouple import config
 import requests
 import json
 
 url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Mae%20Chan?unitGroup=metric&include=current&key=ASRQL55QSXYKL63Y2DCEMFUC3&contentType=json'
+
 response = requests.get(url)
 text = response.text
-
-with open('json.json', 'w') as f:
-    f.write(text)
 
 weather_data = json.loads(text)
 #print(weather_data["currentConditions"])
@@ -14,4 +13,4 @@ weather_data = json.loads(text)
 current_conditions = weather_data["currentConditions"]
 temperature = current_conditions["temp"]
 
-print(f'\nTemperature: {temperature:.0f}°C')
+print(f'{temperature:.0f}°C')
